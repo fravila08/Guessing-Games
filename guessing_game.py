d74e84bbfc648b4e811157ecb71c719d59306ab6
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-31 12:01:18 -0700

    DONE

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,24 @@
-class GuessingGame():
+class GuessingGame:
+    def __init__(self, answer):
+        self.answer=answer  
+    
+    def guess(self,gues=None,solved=False):
+        self.gues=gues
+        self.solved=solved
+        if self.gues==None:
+            return solved
+        elif self.gues>self.answer:
+            return 'high'
+        elif self.gues<self.answer:
+            return 'low'
+        elif self.gues==self.answer:
+            self.solved= True
+            return 'correct'
+        
+#game=GuessingGame(10)
+#print(game.answer)
+#print(game.guess(15))
+#print(game.guess(5))
+#print(game.solved)
+#print(game.guess(10), game.solved)
+#print(game.guess())
